79f8b451b38e4314906026ee2e6b4e42a6565739
KIMBERLY WANG ZI YUN 2300955H <2300955H@student.tp.edu.sg>
2024-12-08 20:09:29 +0800

    Added seed role and user

--- PetAdoptionManagement/Data/PetAdoptionManagementUser.cs
+++ PetAdoptionManagement/Data/PetAdoptionManagementUser.cs
@@ -5,5 +5,7 @@ namespace PetAdoptionManagement.Data
     // Add profile data for application users by adding properties to the ApplicationUser class
     public class PetAdoptionManagementUser : IdentityUser
     {
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
     }
 }
