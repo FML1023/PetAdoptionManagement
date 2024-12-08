9a3f1159e081eece577ea9f3e792cb5ea24f271a
KIMBERLY WANG ZI YUN 2300955H <2300955H@student.tp.edu.sg>
2024-12-08 20:17:59 +0800

    Added custom identity logic

--- PetAdoptionManagement/Program.cs
+++ PetAdoptionManagement/Program.cs
@@ -34,6 +34,7 @@ builder.Services.AddAuthentication(options =>
     .AddIdentityCookies();
 
 builder.Services.AddIdentityCore<PetAdoptionManagementUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddRoles<IdentityRole>()
     .AddEntityFrameworkStores<PetAdoptionManagementContext>()
     .AddSignInManager()
     .AddDefaultTokenProviders();
